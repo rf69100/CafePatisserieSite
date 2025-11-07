ba81582bde16484115c515106639af51a72ca031
rf69100 <fonseca.ryan69100@gmail.com>
2025-11-07 19:49:30 +0100

    version focntionnelle

--- deploy.sh
+++ deploy.sh
@@ -20,7 +20,7 @@ fi
 PROJECT_LIST=(
   "Portfolio:/var/www/html/websites/react/mon-portfolio::build"
   "Café Pâtisserie:/var/www/html/websites/react/CafePatisserieSite:cafe-patisserie:dist/public"
-  # "NBA Dashboard:/var/www/html/websites/react/nba-dashbord:nba_dashboard:nba_dashboard"
+  # "NBA Dashboard:/var/www/html/websites/react/nba-dashbord:nba_dashboard:dist"
   # "Spotify Album Finder:/var/www/html/websites/react/album_finder_spotify:spotify-finder:dist"
   # "F1 Strategy Simulator:/var/www/html/websites/react/f1-strategy-simulator:f1-simulator:dist"
 )
